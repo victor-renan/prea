4395c656ac163da5e8c16de4b5cf071a83ae8fa1
renan <alvesrenanpro@gmail.com>
2024-04-14 09:08:20 -0300

    chore: changed dir from midleware to helpers

--- internal/helpers/responses.go
+++ internal/helpers/responses.go
@@ -1,4 +1,4 @@
-package middlewares
+package helpers
 
 import "github.com/gin-gonic/gin"
 
--- internal/preas/user/user_controller.go
+++ internal/preas/user/user_controller.go
@@ -3,7 +3,7 @@ package user
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"prea/internal/middlewares"
+	"prea/internal/helpers"
 )
 
 type UserController struct {
@@ -20,62 +20,62 @@ func (bc UserController) ForEngine(router *gin.Engine) {
 		users.GET("/", func(ctx *gin.Context) {
 			objs, err := bc.Service.GetAll()
 			if err != nil {
-				middlewares.WarnRes(ctx, err.Error(), http.StatusInternalServerError)
+				helpers.WarnRes(ctx, err.Error(), http.StatusInternalServerError)
 				return
 			}
 
-			middlewares.DataRes(ctx, objs, http.StatusOK)
+			helpers.DataRes(ctx, objs, http.StatusOK)
 		})
 
 		users.GET("/:id", func(ctx *gin.Context) {
 			obj, err := bc.Service.GetById(ctx.Param("id"))
 			if err != nil {
-				middlewares.WarnRes(ctx, err.Error(), http.StatusNotFound)
+				helpers.WarnRes(ctx, err.Error(), http.StatusNotFound)
 				return
 			}
 
-			middlewares.DataRes(ctx, obj, http.StatusOK)
+			helpers.DataRes(ctx, obj, http.StatusOK)
 		})
 
 		users.PUT("/", func(ctx *gin.Context) {
 			var entity UserCreateDAO
 			if err := ctx.ShouldBind(&entity); err != nil {
-				middlewares.WarnRes(ctx, err.Error(), http.StatusNotFound)
+				helpers.WarnRes(ctx, err.Error(), http.StatusNotFound)
 				return
 			}
 
 			obj, err := bc.Service.Create(entity)
 			if err != nil {
-				middlewares.WarnRes(ctx, err.Error(), http.StatusInternalServerError)
+				helpers.WarnRes(ctx, err.Error(), http.StatusInternalServerError)
 				return
 			}
 
-			middlewares.DataRes(ctx, obj, http.StatusOK)
+			helpers.DataRes(ctx, obj, http.StatusOK)
 		})
 
 		users.PATCH("/:id", func(ctx *gin.Context) {
 			var entity UserUpdateDAO
 			if err := ctx.ShouldBind(&entity); err != nil {
-				middlewares.WarnRes(ctx, err.Error(), http.StatusNotFound)
+				helpers.WarnRes(ctx, err.Error(), http.StatusNotFound)
 				return
 			}
 
 			obj, err := bc.Service.Update(ctx.Param("id"), entity)
 			if err != nil {
-				middlewares.WarnRes(ctx, err.Error(), http.StatusNotFound)
+				helpers.WarnRes(ctx, err.Error(), http.StatusNotFound)
 				return
 			}
 
-			middlewares.DataRes(ctx, obj, http.StatusOK)
+			helpers.DataRes(ctx, obj, http.StatusOK)
 		})
 
 		users.DELETE("/:id", func(ctx *gin.Context) {
 			if err := bc.Service.Delete(ctx.Param("id")); err != nil {
-				middlewares.WarnRes(ctx, err.Error(), http.StatusNotFound)
+				helpers.WarnRes(ctx, err.Error(), http.StatusNotFound)
 				return
 			}
 
-			middlewares.SuccRes(ctx, "Item deletado com sucesso!", http.StatusOK)
+			helpers.SuccRes(ctx, "Item deletado com sucesso!", http.StatusOK)
 		})
 	}
 }
